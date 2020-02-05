5c3748e1bcc208bce710994cc8cceaca1e440a36
Frederic Wollinger <fredericwollinger@gmail.com>
2020-02-05 21:07:55 +0100

    minor fix

--- packages/frontend/src/windows/UserWindows/UserHelperFunctions.tsx
+++ packages/frontend/src/windows/UserWindows/UserHelperFunctions.tsx
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import { authURL, baseURL } from '../../data';
+import { throwDataSucess, throwDataError } from '../../pages/User/userHelperFunctions';
 
-// export const login = async (username: string, password: string): Promise<void> => {
-//     return await axios
-//         .post<string>(`${baseURL}${authURL}/login`, { username, password })
-//         .then((response) => {
-//             sessionStorage.setItem('auth', response.data);
-//             return response;
-//         })
-//         .catch((error) => {
-//             return error;
-//         });
-// };
+export const login = async (username: string, password: string): Promise<string> => {
+    try {
+        const res = await axios.post<string>(`${baseURL}${authURL}/login`, { username, password });
+        sessionStorage.setItem('auth', res.data);
+        throwDataSucess('Logged in')
+        return res.data;
+    } catch(e) {
+        throwDataError('Cant login', e);
+        throw e;
+    };
+};
